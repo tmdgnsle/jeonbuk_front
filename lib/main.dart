de69502e5f6068e936d09222e5e5b49fd31e8572
seunghoon <rhfqoddl555@gmail.com>
2024-06-07 17:07:14 +0900

    google analytics 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'dart:async';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:jeonbuk_front/cubit/id_jwt_cubit.dart';
+import 'package:jeonbuk_front/firebase_options.dart';
 import 'package:jeonbuk_front/screen/login_screen.dart';
-import 'dart:developer';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
     debugPrint('Flutter Error: ${details.exceptionAsString()}');
   };
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print('firebase 연결');
 
   await NaverMapSdk.instance.initialize(
       clientId: 'nf68z75anv',
@@ -31,6 +38,10 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -43,6 +54,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'JeonBuk',
         home: const LoginScreen(),
+        navigatorObservers: <NavigatorObserver>[observer],
       ),
     );
   }
